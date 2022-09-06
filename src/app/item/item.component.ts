9c74a9fcff601287c89f85fac57aaa6926a2f9a2
slim <slimcodes@proton.me>
2022-09-06 23:27:50 +0100

    Add Input decorators to item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -5,12 +5,23 @@ import { Item } from '../item';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css'],
 })
+
 /* SECTION
 * should implements OnInIt and constructor be removed?
   are they relevant for this project?
-* /
+*/
 export class ItemComponent implements OnInit {
-  editable = false;
+  editable = true;
+  /* REVIEW: why are both inputs returning error?? */
+  @Input() item!: Item;
+  @Input() newItem!: string;
+  @Output() remove = new EventEmitter<Item>();
+
+  saveItem(description: string) {
+    if (!description) return;
+    this.editable = true;
+    this.item.description = description;
+  }
   constructor() {}
 
   ngOnInit(): void {}
