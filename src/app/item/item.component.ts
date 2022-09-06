93abf5bdc07518fa4432a5750e247f0fe017f62e
slim <slimcodes@proton.me>
2022-09-06 22:28:31 +0100

    Import item interface to Item component

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Item } from '../item';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
