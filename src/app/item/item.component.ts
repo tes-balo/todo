77c32da2d800b433fd4c58cb7e610a0c9d056de5
slim <slimcodes@proton.me>
2022-09-06 22:32:47 +0100

    Add relevant comment to `Item Component`

--- src/app/item/item.component.ts
+++ src/app/item/item.component.ts
@@ -5,7 +5,12 @@ import { Item } from '../item';
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css'],
 })
+/* SECTION
+* should implements OnInIt and constructor be removed?
+  are they relevant for this project?
+* /
 export class ItemComponent implements OnInit {
+  editable = false;
   constructor() {}
 
   ngOnInit(): void {}
